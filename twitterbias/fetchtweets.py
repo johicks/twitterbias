3f3ab1308165cb1ef98291684d0da9e11100fcf7
Josh Hicks <josh.hicks@live.com>
2017-05-16 14:49:39 -0700

    Cleanup fetchtweets.py and create new DB entries only if they do not exist

--- twitterbias/fetchtweets.py
+++ twitterbias/fetchtweets.py
@@ -1,35 +1,30 @@
+import logging
 import twitterclient
 import tweetorm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///sqlalchemy_tweets.db')
-
 tweetorm.Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
-
 session = DBSession()
 
-TC = twitterclient.TwitterClient()
-tweets = TC.api.user_timeline(screen_name='realDonaldTrump', count=200)
-for tweet in tweets:
-    new_tweet = tweetorm.Trump(
-        created_at=tweet.created_at,
-        text=TC.clean_tweet(tweet.text),
-        tweet_id=tweet.id
-        )
+def create_if_not_exists(session, twitteruser, model):
+    TC = twitterclient.TwitterClient()
+    tweets = TC.api.user_timeline(screen_name=twitteruser, count=200)
+    for tweet in tweets:
+        instance = session.query(model).filter(model.tweet_id == tweet.id).one()
+        if instance:
+            logging.debug('Tweet ID exists in DB, skipping: %s', tweet.id)
+        else:
+            new_tweet = model(
+                created_at=tweet.created_at,
+                text=TC.clean_tweet(tweet.text),
+                tweet_id=tweet.id
+                )
+            session.add(new_tweet)
+            session.commit()
 
-    session.add(new_tweet)
-    session.commit()
-
-tweets = TC.api.user_timeline(screen_name='HillaryClinton', count=200)
-for tweet in tweets:
-    new_tweet = tweetorm.Clinton(
-        created_at=tweet.created_at,
-        text=TC.clean_tweet(tweet.text),
-        tweet_id=tweet.id
-        )
-
-    session.add(new_tweet)
-    session.commit()
+if __name__ == '__main__':            
+    create_if_not_exists(session, 'realDonaldTrump', tweetorm.Trump)
+    create_if_not_exists(session, 'HillaryClinton', tweetorm.Clinton)
--- twitterbias/tweetorm.py
+++ twitterbias/tweetorm.py
@@ -8,19 +8,17 @@ Base = declarative_base()
 
 class Trump(Base):
     __tablename__ = 'trump'
-    id = Column(Integer, primary_key=True)
+    tweet_id = Column(Integer, primary_key=True)
     created_at = Column(DateTime())
     text = Column(String(250), nullable=False)
     semantic_score = Column(Integer)
-    tweet_id = Column(Integer)
 
 class Clinton(Base):
     __tablename__ = 'clinton'
-    id = Column(Integer, primary_key=True)
+    tweet_id = Column(Integer, primary_key=True)
     created_at = Column(DateTime())
     text = Column(String(250), nullable=False)
     semantic_score = Column(Integer)
-    tweet_id = Column(Integer)
 
 engine = create_engine('sqlite:///sqlalchemy_tweets.db')
 
