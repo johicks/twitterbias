6349e6e06555d6bf0d10f73017e5bc4fd2b56524
Josh Hicks <josh.hicks@live.com>
2017-06-06 16:01:54 -0700

    Fix tweetorm test

--- tests/test_tweetorm.py
+++ tests/test_tweetorm.py
@@ -6,6 +6,7 @@ import sqlalchemy
 import twitterbias.tweetorm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists
 
 
 def test_tweetorm():
@@ -13,6 +14,7 @@ def test_tweetorm():
     twitterbias.tweetorm.Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
+    twitterbias.tweetorm.Base.metadata.create_all(engine)
     assert os.path.isfile('sqlalchemy_tweets.db')
     os.remove('sqlalchemy_tweets.db')
 
